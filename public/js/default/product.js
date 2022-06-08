db15a78d942de12e714c1558df68e1b6e55290cf
chidleev <chidleev22@gmail.com>
2022-06-08 16:22:28 +0300

    08.06.22 16.22

--- public/js/default/product.js
+++ public/js/default/product.js
@@ -1,5 +1,61 @@
 export default function (htmlPage) {
     return {
-        template: htmlPage
+        template: htmlPage,
+        props: {
+            categoryENname: '',
+            productName: ''
+        },
+        data() {
+            return {
+                product: {
+                    dimensions: [],
+                    materials: []
+                }
+            }
+        },
+        mounted() {
+            window.addEventListener('productsDistribution', this.gettingProduct)
+            window.addEventListener('categoriesDistribution', this.reqProduct)
+
+            window.dispatchEvent(new Event('categoriesRequest'))
+
+            var onAppend = function (elem, f) {
+                var observer = new MutationObserver(function (mutations) {
+                    mutations.forEach(function (m) {
+                        if (m.addedNodes.length) {
+                            f(m.addedNodes)
+                        }
+                    })
+                })
+                observer.observe(elem, { childList: true })
+            }
+
+            onAppend(document.querySelector('.carousel'), function (added) {
+                var carousel = document.querySelector('.carousel');
+                var flkty = new Flickity(carousel, {
+                    wrapAround: true,
+                    autoPlay: true,
+                    imagesLoaded: true,
+                    percentPosition: false,
+                });
+            })
+        },
+        beforeDestroy() {
+            window.removeEventListener('productsDistribution', this.gettingProduct)
+            window.removeEventListener('categoriesDistribution', this.reqProduct)
+        },
+        methods: {
+            gettingProduct(event) {
+                this.product = event.detail.products.find(product => product.name == this.$route.params.productName)
+            },
+            reqProduct(event) {
+                window.dispatchEvent(new CustomEvent('productsRequest', {
+                    detail: {
+                        categoryENname: this.$route.params.categoryENname,
+                        searchLine: this.$route.params.productName
+                    }
+                }))
+            }
+        }
     }
 }
